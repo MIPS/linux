dc0f737c7687d369b3617747d43b09eb56ba150e
James Hogan <james.hogan@imgtec.com>
2015-02-27 12:24:56 +0000

    FIXME: MIPS: Correct __UA_LIMIT for KVM guests
    __UA_LIMIT is a mask of bits that should not be set in userland
    addresses. In KVM guests it is only used for validating ptrace
    watchpoint addresses (which aren't emulated), and not for the USER_DS
    segment mask.
    
    Technically it is being set slightly incorrectly for KVM guests, as
    0x40000000 allows the high bit to be set. Lets correct it in case we
    support guest watchpoints in future host kernels.
    
    This also allows us to share USER_DS and KERNEL_DS definitions between
    normal kernels and KVM guest kernels. It should be harmless for the
    KERNEL_DS mask to be 0 instead of 0x80000000, as accesses from the guest
    kernel should cause a guest exception anyway.
    
    FIXME is that actually the case, surely guest user could crash guest
    kernel by tricking it into dereferencing such an address and it won't
    handle the address error exception that results.
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>

--- arch/mips/include/asm/uaccess.h
+++ arch/mips/include/asm/uaccess.h
@@ -28,7 +28,7 @@
 #ifdef CONFIG_32BIT
 
 #ifdef CONFIG_KVM_GUEST
-#define __UA_LIMIT 0x40000000UL
+#define __UA_LIMIT 0xC0000000UL
 #else
 #define __UA_LIMIT 0x80000000UL
 #endif
@@ -63,13 +63,8 @@ extern u64 __ua_limit;
  * address in this range it's the process's problem, not ours :-)
  */
 
-#ifdef CONFIG_KVM_GUEST
-#define KERNEL_DS	((mm_segment_t) { 0x80000000UL })
-#define USER_DS		((mm_segment_t) { 0xC0000000UL })
-#else
 #define KERNEL_DS	((mm_segment_t) { 0UL })
 #define USER_DS		((mm_segment_t) { __UA_LIMIT })
-#endif
 
 #define VERIFY_READ    0
 #define VERIFY_WRITE   1
