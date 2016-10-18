90357ae6a2b08c053948b5f816f47b3d32320192
Paul Burton <paul.burton@imgtec.com>
2016-10-18 15:44:30 +0100

    irqchip: mips-gic: Use num_possible_cpus() to reserve IPIs
    When reserving GIC interrupts for use as IPIs it makes much more sense
    to use num_possible_cpus() to determine the number of IPIs to reserve.
    This allows us to not reserve IPIs for more CPUs than the kernel
    supports - eg. if we use a kernel with a lower NR_CPUS than the actual
    count of CPUs in the system. It also allows us to automatically gain
    support for counting CPUs across clusters, which the SMP implementation
    will handle.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>

--- drivers/irqchip/irq-mips-gic.c
+++ drivers/irqchip/irq-mips-gic.c
@@ -964,10 +964,10 @@ static void __init __gic_init(unsigned long gic_base_addr,
 	    !of_property_read_u32_array(node, "mti,reserved-ipi-vectors", v, 2)) {
 		bitmap_set(ipi_resrv, v[0], v[1]);
 	} else {
-		/* Make the last 2 * gic_vpes available for IPIs */
+		/* Make the last 2 * num_possible_cpus() available for IPIs */
 		bitmap_set(ipi_resrv,
-			   gic_shared_intrs - 2 * gic_vpes,
-			   2 * gic_vpes);
+			   gic_shared_intrs - 2 * num_possible_cpus(),
+			   2 * num_possible_cpus());
 	}
 
 	bitmap_copy(ipi_available, ipi_resrv, GIC_MAX_INTRS);
