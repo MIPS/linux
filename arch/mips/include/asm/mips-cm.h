f661850fd71682c932b50b0c56519db9d9a6b67a
Paul Burton <paul.burton@imgtec.com>
2016-10-18 12:49:49 +0100

    MIPS: CM: Introduce CM3.5 & multi-cluster definitions & utilities
    This patch introduces definitions & utility functions for the MIPS
    Coherence Manager version 3.5 & the notion of multiple clusters that it
    introduces.
    
    The core-other & VP-other regions from prior versions of the CM have
    developed into "redirect" regions and can now access shared/global
    registers in addition to core or VP local registers, allowing for
    cross-cluster register access. Definitions for the CM registers
    controlling these regions are introduced by this patch.
    
    The GlobalNumber register includes a cluster field, and we introduce
    cpu_cluster() & cpu_set_cluster() accessors for that field in order to
    allow us to track which cluster a CPU belongs to.
    
    The GCR_CONFIG register introduces a new cluster field, which we also
    define & probe a mips_cm_numclusters() accessor for.
    
    On top of these basics we build a couple of utility functions & macros.
    mips_cm_using_multicluster() allows its caller to determine whether
    multiple clusters are in use by the system, allowing for different paths
    to be taken depending upon whether clusters need to be taken into
    account. for_each_possible_cluster() allows us to loop through each
    cluster that a possible CPU can be found in.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>

--- arch/mips/include/asm/mips-cm.h
+++ arch/mips/include/asm/mips-cm.h
@@ -230,6 +230,8 @@ BUILD_CM_Cx_R_(tcid_7_priority,	0x78)
 BUILD_CM_Cx_R_(tcid_8_priority,	0x80)
 
 /* GCR_CONFIG register fields */
+#define CM3_GCR_CONFIG_NUMCLUSTERS_SHF		23
+#define CM3_GCR_CONFIG_NUMCLUSTERS_MSK		(_ULCAST_(0x3f) << 23)
 #define CM_GCR_CONFIG_NUMIOCU_SHF		8
 #define CM_GCR_CONFIG_NUMIOCU_MSK		(_ULCAST_(0xf) << 8)
 #define CM_GCR_CONFIG_PCORES_SHF		0
@@ -266,6 +268,7 @@ BUILD_CM_Cx_R_(tcid_8_priority,	0x80)
 #define CM_REV_CM2				CM_ENCODE_REV(6, 0)
 #define CM_REV_CM2_5				CM_ENCODE_REV(7, 0)
 #define CM_REV_CM3				CM_ENCODE_REV(8, 0)
+#define CM_REV_CM3_5				CM_ENCODE_REV(9, 0)
 
 /* GCR_ERR_CONTROL register fields */
 #define CM_GCR_ERR_CONTROL_L2_ECC_EN_SHF	1
@@ -377,6 +380,14 @@ BUILD_CM_Cx_R_(tcid_8_priority,	0x80)
 /* GCR_Cx_OTHER register fields */
 #define CM_GCR_Cx_OTHER_CORENUM_SHF		16
 #define CM_GCR_Cx_OTHER_CORENUM_MSK		(_ULCAST_(0xffff) << 16)
+#define CM3_GCR_Cx_REDIRECT_CLUSTER_REDIREN_SHF	31
+#define CM3_GCR_Cx_REDIRECT_CLUSTER_REDIREN_MSK	(_ULCAST_(0x1) << 31)
+#define CM3_GCR_Cx_REDIRECT_GIC_REDIREN_SHF	30
+#define CM3_GCR_Cx_REDIRECT_GIC_REDIREN_MSK	(_ULCAST_(0x1) << 30)
+#define CM3_GCR_Cx_REDIRECT_BLOCK_SHF		24
+#define CM3_GCR_Cx_REDIRECT_BLOCK_MSK		(_ULCAST_(0x3) << 24)
+#define CM3_GCR_Cx_REDIRECT_CLUSTER_SHF		16
+#define CM3_GCR_Cx_REDIRECT_CLUSTER_MSK		(_ULCAST_(0x3f) << 16)
 #define CM3_GCR_Cx_OTHER_CORE_SHF		8
 #define CM3_GCR_Cx_OTHER_CORE_MSK		(_ULCAST_(0x3f) << 8)
 #define CM3_GCR_Cx_OTHER_VP_SHF			0
@@ -504,6 +515,117 @@ static inline unsigned int mips_cm_vp_id(unsigned int cpu)
 
 	return (core * mips_cm_max_vp_width()) + vp;
 }
+
+/**
+ * mips_cm_numclusters() - return the number of clusters present in the system
+ *
+ * Returns the value of the NUM_CLUSTERS field of the GCR_CONFIG register where
+ * implemented, or 1 if the system doesn't support clusters or no Coherence
+ * Manager is present.
+ */
+static inline unsigned int mips_cm_numclusters(void)
+{
+	unsigned int cfg;
+
+	if (mips_cm_revision() < CM_REV_CM3_5)
+		return 1;
+
+	cfg = read_gcr_config();
+	cfg &= CM3_GCR_CONFIG_NUMCLUSTERS_MSK;
+	cfg >>= CM3_GCR_CONFIG_NUMCLUSTERS_SHF;
+
+	return cfg;
+}
+
+/**
+ * mips_cm_using_multicluster() - determine whether multiple clusters are in use
+ *
+ * Returns true if the system is using multiple clusters, otherwise false. This
+ * is useful for callers that can act more optimally if they know whether they
+ * need to act upon multiple clusters or not.
+ */
+static inline bool mips_cm_using_multicluster(void)
+{
+	unsigned int last_cpu;
+
+	/*
+	 * We rely upon CPUs being probed in each cluster in order, with CPUs
+	 * in secondary clusters coming after the boot cluster (cluster 0). This
+	 * means that we can determine whether multiple clusters are in use purely
+	 * by examining whether the last possible CPU is in the boot cluster.
+	 */
+	last_cpu = find_last_bit(cpumask_bits(cpu_possible_mask), nr_cpumask_bits);
+	return cpu_cluster(&cpu_data[last_cpu]) != 0;
+}
+
+/**
+ * __mips_cm_first_cluster() - Find the first cluster number from a cpumask
+ * @cpumask: mask containing CPUs whose clusters we want to cover
+ *
+ * Find the cluster number for the first CPU set in @cpumask. Not intended for
+ * direct use - instead make use of this via the for_each_possible_cluster()
+ * macro.
+ *
+ * Return: the cluster number of the first CPU set in @cpumask
+ */
+static inline unsigned int
+__mips_cm_first_cluster(const struct cpumask *cpumask)
+{
+	return cpu_cluster(&cpu_data[cpumask_first(cpumask)]);
+}
+
+/**
+ * __mips_cm_next_cluster() - Find the next cluster covering a cpumask
+ * @cpumask: mask containing CPUs whose clusters we want to cover
+ * @prev: the cluster to start from
+ *
+ * Find the cluster number for the cluster following @prev which contains CPUs
+ * set within @cpumask. Not intended for direct use - instead make use of this
+ * via the for_each_possible_cluster() macro.
+ *
+ * Return: the cluster number following @prev, or UINT_MAX if no more clusters
+ */
+static inline unsigned int
+__mips_cm_next_cluster(const struct cpumask *cpumask, unsigned int prev)
+{
+	unsigned int cpu;
+
+	/*
+	 * We rely here upon having probed CPUs from each cluster sequentially
+	 * with a strictly incrementing cluster number. That is, each CPU
+	 * should have a cluster number greater or equal than that of all CPUs
+	 * with a lower CPU number.
+	 */
+	for_each_cpu(cpu, cpumask) {
+		if (cpu_cluster(&cpu_data[cpu]) <= prev)
+			continue;
+
+		return cpu_cluster(&cpu_data[cpu]);
+	}
+
+	return UINT_MAX;
+}
+
+/*
+ * for_each_possible_cluster() - Loop over clusters containing possible CPUs
+ * @cluster: an unsigned integer to contain the cluster number
+ *
+ * Loop over all clusters which contain any CPUs set in cpu_possible_mask. This
+ * can be used to easily operate on all clusters that Linux is running across.
+ * For example you may access a register in all clusters by doing something
+ * along the lines of:
+ *
+ *   unsigned int cluster;
+ *   for_each_possible_cluster(cluster) {
+ *     mips_cm_lock_other(cluster, 0, 0, BLOCK_GCR_GLOBAL);
+ *     write_redir_gcr_gic_base(0x10000001);
+ *     mips_cm_unlock_other();
+ *   }
+ */
+#define for_each_possible_cluster(cluster)					\
+	for ((cluster) = __mips_cm_first_cluster(cpu_possible_mask);		\
+	     (cluster) != UINT_MAX;						\
+	     (cluster) = __mips_cm_next_cluster(cpu_possible_mask, cluster))
 
 #ifdef CONFIG_MIPS_CM
 
