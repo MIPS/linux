c5ca46277a55d9f7647b4d9d0887a5e99b519dcb
Paul Burton <paul.burton@imgtec.com>
2017-08-30 15:42:44 -0700

    net: pch_gbe: Remove init_hw HAL abstraction
    The pch_gbe driver provides a HAL abstraction for hardware
    initialisation in the form of pch_gbe_hal_init_hw(), which calls an
    init_hw function pointer in struct pch_gbe_functions. There is only one
    implementation of this function, and only one caller. Remove the
    needless HAL abstraction & inline the pch_gbe_plat_init_hw()
    implementation into pch_gbe_reset().
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>

--- drivers/net/ethernet/oki-semi/pch_gbe/pch_gbe_api.h
+++ drivers/net/ethernet/oki-semi/pch_gbe/pch_gbe_api.h
@@ -22,7 +22,6 @@
 #include "pch_gbe_phy.h"
 
 s32 pch_gbe_hal_setup_init_funcs(struct pch_gbe_hw *hw);
-s32 pch_gbe_hal_init_hw(struct pch_gbe_hw *hw);
 s32 pch_gbe_hal_read_phy_reg(struct pch_gbe_hw *hw, u32 offset, u16 *data);
 s32 pch_gbe_hal_write_phy_reg(struct pch_gbe_hw *hw, u32 offset, u16 data);
 void pch_gbe_hal_phy_hw_reset(struct pch_gbe_hw *hw);
