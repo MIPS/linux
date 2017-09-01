2aa847422d57c475efb62cd99025b388491e2765
Paul Burton <paul.burton@imgtec.com>
2017-09-01 10:53:47 -0700

    net: pch_gbe: Ensure DMA is ordered with descriptor writes
    On weakly ordered systems writes to the RX or TX descriptors may be
    reordered with the write to the DMA control register that enables DMA.
    If this happens then the device may see descriptors in an intermediate
    & invalid state, leading to incorrect behaviour. Add barriers to ensure
    that DMA is enabled only after all writes to the descriptors.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>

--- drivers/net/ethernet/oki-semi/pch_gbe/pch_gbe_main.c
+++ drivers/net/ethernet/oki-semi/pch_gbe/pch_gbe_main.c
@@ -1280,6 +1280,9 @@ static void pch_gbe_tx_queue(struct pch_gbe_adapter *adapter,
 	tx_desc->tx_frame_ctrl = cpu_to_le16(frame_ctrl);
 	tx_desc->gbec_status = cpu_to_le16(DSC_INIT16);
 
+	/* Ensure writes to descriptors complete before DMA begins */
+	mmiowb();
+
 	if (unlikely(++ring_num == tx_ring->count))
 		ring_num = 0;
 
@@ -1991,6 +1994,9 @@ int pch_gbe_up(struct pch_gbe_adapter *adapter)
 	pch_gbe_alloc_tx_buffers(adapter, tx_ring);
 	pch_gbe_alloc_rx_buffers(adapter, rx_ring, rx_ring->count);
 	adapter->tx_queue_len = netdev->tx_queue_len;
+
+	/* Ensure writes to descriptors complete before DMA begins */
+	mmiowb();
 
 	pch_gbe_enable_dma_tx(&adapter->hw);
 	pch_gbe_enable_dma_rx(&adapter->hw);
