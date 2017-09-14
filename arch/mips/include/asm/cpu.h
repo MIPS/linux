9293cf0aec0a01c7e7bb76d54bf71c79ecf878cb
James Hogan <james.hogan@imgtec.com>
2017-09-14 10:45:06 +0100

    MIPS: Big pages probing & accessors
    The big pages architectural feature allows for page sizes larger than
    256MiB to be supported by MIPS64 hardware and indicates that the
    CP0_PageMask register is 64-bits wide.
    
    Add the CPU feature macros, probing of the CP0_Config3.BPG bit and the
    corresponding 64-bit CP0_PageMask accessors, for both the root and guest
    contexts.
    
    64-bit access to [Guest.]CP0_PageMask should be used only when
    cpu_[guest_]has_big_pages.
    
    This will be used by KVM to correctly save/restore/expose the full guest
    CP0_PageMask register when BPG is set.
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: linux-mips@linux-mips.org

--- arch/mips/include/asm/cpu.h
+++ arch/mips/include/asm/cpu.h
@@ -418,6 +418,7 @@ enum cpu_type_enum {
 				MBIT_ULL(54)	/* CPU shares FTLB RAM with another */
 #define MIPS_CPU_SHARED_FTLB_ENTRIES \
 				MBIT_ULL(55)	/* CPU shares FTLB entries with another */
+#define MIPS_CPU_BPG		MBIT_ULL(56)	/* Big (>256MB) page support */
 
 /*
  * CPU ASE encodings
