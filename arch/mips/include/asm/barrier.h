736bc20b4cea0065220b20ccdfaa0d776890d32f
Matt Redfearn <matt.redfearn@mips.com>
2017-12-20 10:38:32 +0000

    MIPS: Move ehb() to barrier.h
    The current location of ehb() in mipsmtregs.h does not make sense, since
    it is not strictly related to multi-threading, and may be used in code
    which does not include mipsmtregs.h
    
    Signed-off-by: Matt Redfearn <matt.redfearn@mips.com>

--- arch/mips/include/asm/barrier.h
+++ arch/mips/include/asm/barrier.h
@@ -222,6 +222,19 @@
 #define __smp_mb__before_atomic()	__smp_mb__before_llsc()
 #define __smp_mb__after_atomic()	smp_llsc_mb()
 
+/**
+ * ehb() - Execution Hazard Barrier
+ *
+ * Stop instruction execution until execution hazards are cleared
+ */
+static inline void ehb(void)
+{
+	__asm__ __volatile__(
+	"	.set	mips32r2				\n"
+	"	ehb						\n"
+	"	.set	mips0					\n");
+}
+
 #include <asm-generic/barrier.h>
 
 #endif /* __ASM_BARRIER_H */
