f5321192370801789ced67c4982e5f9bcbf9bafb
Paul Burton <paul.burton@mips.com>
2018-03-01 15:43:55 -0800

    MIPS: nanoMIPS uprobes support
    TODO: unpick into a few patches
    TODO: emulate other pc-relative instructions
    
    Signed-off-by: Paul Burton <paul.burton@mips.com>

--- arch/mips/include/asm/uprobes.h
+++ arch/mips/include/asm/uprobes.h
@@ -11,31 +11,48 @@
 
 #include <asm/break.h>
 #include <asm/inst.h>
+#include <asm/mipsregs.h>
 
 /*
  * We want this to be defined as union mips_instruction but that makes the
  * generic code blow up.
  */
+#ifdef __nanomips__
+typedef struct { u16 h[3]; } uprobe_opcode_t;
+
+static inline bool uprobe_opcode_equal(uprobe_opcode_t a, uprobe_opcode_t b)
+{
+	unsigned int i;
+
+	for (i = 0; i < (nanomips_insn_len(a.h[0]) / 2); i++) {
+		if (a.h[i] != b.h[i])
+			return false;
+	}
+
+	return true;
+}
+# define uprobe_opcode_equal uprobe_opcode_equal
+
+# define UPROBE_MAX_XOL_INSNS 1
+
+# define UPROBE_XOLBREAK_INSN		((uprobe_opcode_t){{ 0x1014 }})	/* break 4 */
+# define UPROBE_SWBP_INSN		((uprobe_opcode_t){{ 0x1013 }})	/* break 3 */
+# define UPROBE_SWBP_INSN_SIZE		2
+#else
 typedef u32 uprobe_opcode_t;
+# define UPROBE_MAX_XOL_INSNS 2
 
-/*
- * Classic MIPS (note this implementation doesn't consider microMIPS yet)
- * instructions are always 4 bytes but in order to deal with branches and
- * their delay slots, we treat instructions as having 8 bytes maximum.
- */
-#define MAX_UINSN_BYTES			8
+# define UPROBE_XOLBREAK_INSN		0x0004000d	/* break 4 */
+# define UPROBE_SWBP_INSN		0x0003000d	/* break 3 */
+# define UPROBE_SWBP_INSN_SIZE		4
+#endif
+
 #define UPROBE_XOL_SLOT_BYTES		128	/* Max. cache line size */
-
-#define UPROBE_BRK_UPROBE		0x000d000d	/* break 13 */
-#define UPROBE_BRK_UPROBE_XOL		0x000e000d	/* break 14 */
-
-#define UPROBE_SWBP_INSN		UPROBE_BRK_UPROBE
-#define UPROBE_SWBP_INSN_SIZE		4
 
 struct arch_uprobe {
 	unsigned long	resume_epc;
-	u32	insn[2];
-	u32	ixol[2];
+	uprobe_opcode_t	insn[UPROBE_MAX_XOL_INSNS];
+	uprobe_opcode_t	ixol[2];
 };
 
 struct arch_uprobe_task {
