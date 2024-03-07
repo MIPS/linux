9e849892866107d2992f741d3fae12e20a6f7d36
Scott Stephens <sstephens@wavecomp.com>
2024-03-07 10:22:42 -0800

    Allow Boston development board custom bridge alignment to allow Intel EG20T PCH bridge to assign memory to child devices using 1MB Xilinx AXI to PCIe root port resources.

--- drivers/pci/setup-bus.c
+++ drivers/pci/setup-bus.c
@@ -941,6 +941,7 @@ resource_size_t __weak pcibios_window_alignment(struct pci_bus *bus,
 #define PCI_P2P_DEFAULT_MEM_ALIGN	SZ_1M
 #define PCI_P2P_DEFAULT_IO_ALIGN	SZ_4K
 #define PCI_P2P_DEFAULT_IO_ALIGN_1K	SZ_1K
+#define PCI_P2P_DEFAULT_MEM_ALIGN	CONFIG_PCI_BRIDGE_MEM_ALIGN	/* default 1MiB */
 
 static resource_size_t window_alignment(struct pci_bus *bus, unsigned long type)
 {
@@ -1059,7 +1060,7 @@ static inline resource_size_t calculate_mem_align(resource_size_t *aligns,
 	for (order = 0; order <= max_order; order++) {
 		resource_size_t align1 = 1;
 
-		align1 <<= order + __ffs(SZ_1M);
+		align1 <<= (order + __ffs(PCI_P2P_DEFAULT_MEM_ALIGN));
 
 		if (!align)
 			min_align = align1;
@@ -1208,7 +1209,7 @@ static int pbus_size_mem(struct pci_bus *bus, unsigned long mask,
 			 * resources.
 			 */
 			align = pci_resource_alignment(dev, r);
-			order = __ffs(align) - __ffs(SZ_1M);
+			order = __ffs(align) - __ffs(PCI_P2P_DEFAULT_MEM_ALIGN);
 			if (order < 0)
 				order = 0;
 			if (order >= ARRAY_SIZE(aligns)) {
